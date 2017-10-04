a3eb66880f44305033ad84e5b6d5d9346be69562
Monika Muzyk <monika.muzyk@hotmail.com>
2017-10-04 22:13:30 +0200

    Remove logo and clean front page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import { version } from 'inferno';
 import Component from 'inferno-component';
 import './registerServiceWorker';
-import Logo from './logo';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <Logo width="80" height="80" />
-          <h2>{`Welcome to Inferno ${version}`}</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <header className="head">
+          <h2>Notiwall v2</h2>
+        </header>
+        <main className="content">
+
+        </main>
       </div>
     );
   }
