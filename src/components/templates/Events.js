990483fc1b331dbdc40a75a03d42945af0cdb4c3
Niklas Molnes Hole <nikasmh@hotmail.com>
2018-02-01 00:58:32 +0100

    Add new events component

---
+++ src/components/templates/Events.js
@@ -0,0 +1,14 @@
+import Component from 'inferno-component';
+import { Template } from '../layout';
+import './Events.css';
+
+class Events extends Component {
+  render() {
+    return (
+      <Template className="Events">
+      </Template>
+    );
+  }
+}
+
+export default Events;
