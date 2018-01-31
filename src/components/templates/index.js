8a6d02776880af19a5d006d2a584207a2b92cb3a
Niklas Molnes Hole <nikasmh@hotmail.com>
2018-02-01 00:57:41 +0100

    Index events component to templates

--- src/components/templates/index.js
+++ src/components/templates/index.js
@@ -1,9 +1,11 @@
 import Bus from './Bus';
+import Events from './Events';
 import Header from './Header';
 import Vakter from './Vakter';
 
 export {
     Bus,
+    Events,
     Header,
     Vakter,
 };
