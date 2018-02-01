23c103c52a6bd0b42d278ff01011c457f64ae721
Niklas Molnes Hole <nikasmh@hotmail.com>
2018-02-01 01:10:36 +0100

    Add a constants file for importing settings from environment variables in frontend

---
+++ src/constants.js
@@ -0,0 +1,3 @@
+export const API_HOST = process.env.INFERNO_APP_API_HOST || '';
+export const API_ROOT = process.env.INFERNO_APP_API_ROOT || 'api/v1';
+export const API_URL = `${API_HOST}/${API_ROOT}`;
