604da577779758f35d3c463f44cb9544704bbf8a
Niklas Molnes Hole <nikasmh@hotmail.com>
2018-03-11 22:46:52 +0100

    Made it possible to see todays coffee

--- src/components/templates/Coffee.js
+++ src/components/templates/Coffee.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Template } from '../layout';
-import { distanceInWordsToNow, format, differenceInHours } from 'date-fns';
+import { distanceInWordsToNow, format, differenceInHours, getDay } from 'date-fns';
 import { get } from 'object-path';
 import * as locale from 'date-fns/locale/nb';
 
@@ -10,31 +10,26 @@ class Coffee extends Component {
 
     this.state = {
       coffeeTime: 0,
+      pots: [],
     };
   }
 
   componentWillReceiveProps(nextProps) {
     let nextDate = get(nextProps, 'data.coffeeTime');
-    let date = get(this.props, 'data.coffeeTime');
 
-    if (nextDate !== date) {
-      if (nextDate === null) {
-        this.setState(Object.assign({}, this.state, {
-          coffeeTime: -1,
-        }));
-      } else {
-        this.setState(Object.assign({}, this.state, {
-          coffeeTime: new Date(nextDate).getTime(),
-        }));
-      }
-    }
+    let state = Object.assign({}, this.state, {
+      coffeeTime: nextDate === null ? -1 : new Date(nextDate).getTime(),
+      pots: get(nextProps, 'data.pots'),
+    });
+
+    this.setState(state);
   }
 
   render() {
-    let coffeeInfo = 'Henter kaffastatus...';
+    let coffeeInfo = 'Henter kaffestatus...';
 
     if (this.state.coffeeTime > 0) {
-      let coffeeDate = new Date(this.state.coffeeTime);
+      let coffeeDate = this.state.coffeeTime;
 
       if (differenceInHours(new Date(), coffeeDate) > 1) {
         let timeFormatted = format(this.state.coffeeTime, 'HH:mm');
@@ -48,9 +43,21 @@ class Coffee extends Component {
       coffeeInfo = 'Kaffen har ikke blitt satt på i dag.';
     }
 
+    let pots = null;
+    if (this.state.pots) {
+      pots = this.state.pots
+      .filter(e => getDay(new Date()) === getDay(e))
+      .map((e, i) => {
+        return <div key={i}>{format(e, 'ddd HH:mm', { locale })}</div>;
+      });
+    }
+
     return (
       <Template className={this.constructor.name} {...this.props}>
+        <h3>Kaffe</h3>
         {coffeeInfo}
+        <h3>Kaffe hittil i dag</h3>
+        {pots}
       </Template>
     );
   }
