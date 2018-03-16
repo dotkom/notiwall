8d0c709f2db24febc2da67bcd2e3310c7c147caf
Niklas Molnes Hole <nikasmh@hotmail.com>
2018-03-16 22:22:37 +0100

    Add test for template string finder

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { findObjectPaths } from './utils';
+import { findObjectPaths, getStringParams } from './utils';
 
 it('Renders without crashing', () => {
   const div = document.createElement('div');
@@ -35,4 +35,13 @@ it('Find object paths', () => {
   expect(findObjectPaths(obj, 'a.b,c.c')).toEqual([ 'a.b.c', 'a.c.c' ]);
   expect(findObjectPaths(obj, 'a.b,c.d')).toEqual([]);
   expect(findObjectPaths(obj, '*.*|a.b,c.c')).toEqual([ 'a.b', 'a.c', 'b.b', 'a.b.c', 'a.c.c' ]);
+});
+
+it('Get string params', () => {
+  expect(getStringParams('test{{one}}')).toEqual([ 'one' ]);
+  expect(getStringParams('{{two}}test')).toEqual([ 'two' ]);
+  expect(getStringParams('{{three}}test{{four}}')).toEqual([ 'three', 'four' ]);
+  expect(getStringParams('{{five}}{{six}}')).toEqual([ 'five', 'six' ]);
+  expect(getStringParams('test{{seven}}test{{eight}}test')).toEqual([ 'seven', 'eight' ]);
+  expect(getStringParams('test{{nine}}{{ten}}')).toEqual([ 'nine', 'ten' ]);
 });
