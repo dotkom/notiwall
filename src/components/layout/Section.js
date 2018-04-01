3329e9c7f3671e5c8d630bf3fbb686d643d1ea97
Niklas Molnes Hole <nikasmh@hotmail.com>
2018-04-01 23:06:11 +0200

    Make section resizeable

--- src/components/layout/Section.js
+++ src/components/layout/Section.js
@@ -3,8 +3,22 @@ import './Section.css';
 
 class Section extends Component {
   render() {
+    let props = Object.assign({}, this.props);
+
+    props.className = `Section ${this.props.className || ''}`;
+
+    if (this.props.column) {
+      props.className += ' column';
+      delete props.column;
+    }
+
+    if (this.props.size) {
+      props.className += ` width-${this.props.size}`;
+      delete props.size;
+    }
+ 
     return (
-      <div className="Section">
+      <div {...props}>
         {this.props.children}
       </div>
     );
