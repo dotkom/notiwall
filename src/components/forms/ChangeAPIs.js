2035d4fbdf0b3bdfa277731dbad459875c7b531e
Niklas Molnes Hole <nikasmh@hotmail.com>
2018-04-03 00:28:10 +0200

    Make it possible to change APIs through the edit interface

--- src/components/forms/ChangeAPIs.js
+++ src/components/forms/ChangeAPIs.js
@@ -1,16 +1,41 @@
 import React, { Component } from 'react';
 import { Section } from '../layout';
+import { getStringParams } from '../../utils';
 
 class ChangeAPIs extends Component {
   render() {
     let { apis, updateApi } = Object.assign({}, this.props);
 
     let apisElement = Object.entries(apis).map(([name, api]) => {
+      let paramsInUrl = getStringParams(api.url);
+      let entries = [];
+
+      if (paramsInUrl.length) {
+        for (let section of paramsInUrl) {
+          entries.push(section.split('.')[0]);
+        }
+      }
+
+      entries = entries.map((e, i) => (
+        <div key={i}>
+          <span>{e}: </span>
+          <textarea defaultValue={JSON.stringify(api[e], null, 2)} />
+        </div>
+      ));
+
       return (
-        <div key={name}>{name} - {api.interval} - {api.url}</div>
+        <div key={name}>
+          <h3>{name}</h3>
+          <input defaultValue={api.interval} type="number" min={0} />
+          <input defaultValue={api.url} />
+          <div>
+            {!!entries.length && <h4>Combinations in URL</h4>}
+            {entries}
+          </div>
+        </div>
       );
     });
- 
+
     return (
       <Section column size={2}>
         <h2>Change APIs</h2>
