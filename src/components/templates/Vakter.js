9007639a211140e5370a2a16c4700a5b061e5f97
Niklas Molnes Hole <nikasmh@hotmail.com>
2018-04-07 15:06:56 +0200

    Make office info match design

--- src/components/templates/Vakter.js
+++ src/components/templates/Vakter.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Template } from '../layout';
 import './Vakter.css';
+import { isAfter, isBefore } from 'date-fns';
 import { get } from 'object-path';
 
 class Vakter extends Component {
@@ -8,39 +9,87 @@ class Vakter extends Component {
     super();
 
     this.state = {
-      message: ' ',
-      responsible: 0,
-      responsibles: [
-        'Emil', 'Karoline', 'Sjokolade', 'Kake', 'Niklas', 'Doge',
+      message: 'Emil',
+      responsible: true,
+      servants: [
+        {
+          "summary":"Emil Karoline",
+          "start":{"timeZone":"Europe/Oslo","date":"2018-04-07T09:00:00.000Z","pretty":"11:00"},
+          "end":{"timeZone":"Europe/Oslo","date":"2018-04-07T10:00:00.000Z","pretty":"12:00"},
+          "pretty":"11:00-12:00"
+        },
+        {
+          "summary":"Karoline Emil",
+          "start":{"timeZone":"Europe/Oslo","date":"2018-04-07T10:00:00.000Z","pretty":"12:00"},
+          "end":{"timeZone":"Europe/Oslo","date":"2018-04-07T11:00:00.000Z","pretty":"13:00"},
+          "pretty":"12:00-13:00"
+        },
+        {
+          "summary":"Sjokolade",
+          "start":{"timeZone":"Europe/Oslo","date":"2018-04-07T11:00:00.000Z","pretty":"13:00"},
+          "end":{"timeZone":"Europe/Oslo","date":"2018-04-07T12:00:00.000Z","pretty":"14:00"},
+          "pretty":"13:00-14:00"
+        },
+        {
+          "summary":"Kake",
+          "start":{"timeZone":"Europe/Oslo","date":"2018-04-07T12:00:00.000Z","pretty":"14:00"},
+          "end":{"timeZone":"Europe/Oslo","date":"2018-04-07T13:00:00.000Z","pretty":"15:00"},
+          "pretty":"14:00-15:00"
+        },
+        {
+          "summary":"Niklas/Doge mac Doge",
+          "start":{"timeZone":"Europe/Oslo","date":"2018-04-07T13:00:00.000Z","pretty":"15:00"},
+          "end":{"timeZone":"Europe/Oslo","date":"2018-04-07T14:00:00.000Z","pretty":"16:00"},
+          "pretty":"15:00-16:00"
+        }
       ]
-    };
-
-    this.templateVars = {
     }
+
+    this.templateVars = {}
   }
 
   componentWillReceiveProps(nextProps) {
-    let nextMsg = get(nextProps, 'message');
-    let nextResponsible = get(nextProps, 'responsible');
+    if (false) {
+      let message = get(nextProps, 'message');
+      let responsible = get(nextProps, 'responsible');
+      let servants = get(nextProps, 'servants');
 
-    let state = Object.assign({}, this.state, {
-      message: nextMsg,
-      responsible: nextResponsible,
-    });
+      let state = Object.assign({}, this.state, {
+        message,
+        responsible,
+        servants,
+      });
 
-    this.setState(state);
+      this.setState(state);
+    }
   }
 
   render() {
     let responsibleListElement = null;
-    if (this.state.responsibles.length){
-      responsibleListElement = this.state.responsibles.map((e, i) => {
-        return <div key={i}>{e}</div>;
-      })
+    if (this.state.responsible && this.state.servants.length){
+      responsibleListElement = this.state.servants.map((e, i) => {
+        let style = {
+          opacity: .5,
+        };
+
+        if (isAfter(new Date(), e.start.date) && isBefore(new Date(), e.end.date)) {
+          style = {
+            color: '#fff',
+          };
+        }
+
+        return <div style={style} key={i}>{e.pretty} - {e.summary}</div>;
+      });
+    } else {
+      responsibleListElement = (
+        <div style={{ fontStyle: 'italic' }}>- Ingen ansvarlige nå</div>
+      );
     }
+
     return (
       <Template className={this.constructor.name} {...this.props}>
-        Vakter her {this.state.message}
+        <h1>Onlinekontoret</h1>
+        <h3>Kontorvakter</h3>
         {responsibleListElement}
       </Template>
     );
