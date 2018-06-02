bc456200c02a58cb5e865ab3ed9209aa09c9fcd7
Niklas Molnes Hole <nikasmh@hotmail.com>
2018-06-02 19:39:17 +0200

    Make the pots number always place itself behind the hour and minute arms

--- src/components/gadgets/Clocks.js
+++ src/components/gadgets/Clocks.js
@@ -14,20 +14,32 @@ export const simpleClock = (time, pots = []) => {
   const second = time.getSeconds();
   const millisecond = time.getMilliseconds();
 
-  const hourX = Math.sin(Math.PI - Math.PI * 2 * (hour / 12 + minute / 12 / 60)) * 16;
-  const hourY = Math.cos(Math.PI + Math.PI * 2 * (hour / 12 + minute / 12 / 60)) * 16;
-  const minuteX = Math.sin(Math.PI - Math.PI * 2 * (minute / 60 + second / 60 / 60)) * 32;
-  const minuteY = Math.cos(Math.PI + Math.PI * 2 * (minute / 60 + second / 60 / 60)) * 32;
-  const secondX = Math.sin(Math.PI - Math.PI * 2 * (second / 60 + millisecond / 60 / 1000)) * 32;
-  const secondY = Math.cos(Math.PI + Math.PI * 2 * (second / 60 + millisecond / 60 / 1000)) * 32;
+  const hourA = Math.PI * 2 * (hour / 12 + minute / 12 / 60);
+  const hourX = Math.sin(Math.PI - hourA) * 16;
+  const hourY = Math.cos(Math.PI + hourA) * 16;
+
+  const minuteA = Math.PI * 2 * (minute / 60 + second / 60 / 60);
+  const minuteX = Math.sin(Math.PI - minuteA) * 32;
+  const minuteY = Math.cos(Math.PI + minuteA) * 32;
+
+  const secondA = Math.PI * 2 * (second / 60 + millisecond / 60 / 1000);
+  const secondX = Math.sin(Math.PI - secondA) * 32;
+  const secondY = Math.cos(Math.PI + secondA) * 32;
 
   const borderSize = 2;
+
+  // Find the angle between two angles
+  const u = hourA;
+  const v = minuteA;
+  const potsA = Math.atan2((Math.sin(u) + Math.sin(v)) / 2, (Math.cos(u) + Math.cos(v)) / 2);
+  const potsX = 50 + Math.sin(- potsA) * 18;
+  const potsY = 50 + Math.cos(potsA) * 18;
 
   return (
     <svg width="100%" height="100%" viewBox="0 0 100 100">
     <circle cx="50" cy="50" r="40" fill="rgba(0, 0, 0, .1)" />
-      <text x="50" y={30} fill="rgba(255, 255, 255, .4)" textAnchor="middle" fontSize="10px">{pots.length}</text>
-      <text x="50" y={38} fill="rgba(255, 255, 255, .4)" textAnchor="middle" fontSize="5px">Pots</text>
+      <text x={potsX} y={potsY - 0} fill="rgba(255, 255, 255, .4)" textAnchor="middle" fontSize="10px">{pots.length}</text>
+      <text x={potsX} y={potsY + 8} fill="rgba(255, 255, 255, .4)" textAnchor="middle" fontSize="5px">Pots</text>
       <line strokeLinecap="round" x1="50" y1="50" x2={50 + hourX} y2={50 + hourY} strokeWidth={borderSize} stroke="rgba(255, 255, 255, 1)" />
       <line strokeLinecap="round" x1="50" y1="50" x2={50 + minuteX} y2={50 + minuteY} strokeWidth={borderSize} stroke="rgba(255, 255, 255, 1)" />
       <line strokeLinecap="round" x1="50" y1="50" x2={50 + secondX} y2={50 + secondY} strokeWidth={borderSize / 2} stroke="rgba(255, 255, 255, .6)" />
