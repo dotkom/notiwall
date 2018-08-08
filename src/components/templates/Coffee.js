3fa459d3f881a0a00be871d217854311bbc0ffa3
Niklas Molnes Hole <nikasmh@hotmail.com>
2018-08-08 17:53:49 +0200

    Render each second instead of using continuos stepping on clock

--- src/components/templates/Coffee.js
+++ src/components/templates/Coffee.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
-import { Template } from '../layout';
-import { distanceInWordsToNow, format, differenceInHours } from 'date-fns';
-import { get } from 'object-path';
-import * as locale from 'date-fns/locale/nb';
-import { simpleClock } from '../gadgets/Clocks';
+import React, { Component } from "react";
+import { Template } from "../layout";
+import { distanceInWordsToNow, format, differenceInHours } from "date-fns";
+import { get } from "object-path";
+import * as locale from "date-fns/locale/nb";
+import { simpleClock } from "../gadgets/Clocks";
 
 class Coffee extends Component {
   constructor() {
@@ -11,11 +11,11 @@ class Coffee extends Component {
 
     this.state = {
       coffeeTime: 0,
-      pots: [],
+      pots: []
     };
 
     this.templateVars = {
-      apis: 'apis',
+      apis: "apis"
     };
 
     this.lastRendered = Date.now();
@@ -29,7 +29,7 @@ class Coffee extends Component {
     const now = Date.now();
     const diff = now - this.lastRendered;
     this.lastRendered = now;
-    const timeout = diff >= 16 ? 0 : 16 - diff;
+    const timeout = diff >= 1000 ? 0 : 1000 - diff;
 
     setTimeout(() => {
       this.forceUpdate();
@@ -37,48 +37,59 @@ class Coffee extends Component {
   }
 
   componentWillReceiveProps(nextProps) {
-    let nextDate = get(nextProps, 'coffeeTime');
+    let nextDate = get(nextProps, "coffeeTime");
 
     let state = Object.assign({}, this.state, {
       coffeeTime: nextDate === null ? -1 : new Date(nextDate).getTime(),
-      pots: get(nextProps, 'pots'),
+      pots: get(nextProps, "pots")
     });
 
     this.setState(state);
   }
 
   render() {
-    let coffeeInfo = 'Henter kaffestatus...';
+    let coffeeInfo = "Henter kaffestatus...";
     let { offline } = this.props;
 
     if (this.state.coffeeTime > 0) {
       let coffeeDate = this.state.coffeeTime;
 
       if (differenceInHours(new Date(), coffeeDate) > 1) {
-        let timeFormatted = format(this.state.coffeeTime, 'HH:mm');
+        let timeFormatted = format(this.state.coffeeTime, "HH:mm");
         coffeeInfo = `Kaffe ble laget kl. ${timeFormatted}`;
       } else {
         let dateFormatted = distanceInWordsToNow(coffeeDate, { locale });
         coffeeInfo = `Kaffe ble laget ${dateFormatted} siden`;
       }
     } else if (this.state.coffeeTime === -1) {
-      coffeeInfo = 'Kaffen har ikke blitt satt på i dag.';
-    } else if (offline && offline.find(a => a.split('.')[0] === 'affiliation')) {
-      coffeeInfo = 'API\'et er utilgjengelig';
+      coffeeInfo = "Kaffen har ikke blitt satt på i dag.";
+    } else if (
+      offline &&
+      offline.find(a => a.split(".")[0] === "affiliation")
+    ) {
+      coffeeInfo = "API'et er utilgjengelig";
     }
 
     let potsList = [];
     if (this.state.pots) {
       potsList = this.state.pots
         .filter(e => differenceInHours(new Date(), e) <= 24)
-        .map(e => format(e, 'HH:mm', { locale }));
+        .map(e => format(e, "HH:mm", { locale }));
     }
 
     return (
-      <Template className={this.constructor.name} {...this.props} templateVars={this.templateVars}>
+      <Template
+        className={this.constructor.name}
+        {...this.props}
+        templateVars={this.templateVars}
+      >
         <h3>Kaffe</h3>
-        {this.props.showCoffeePots !== 'false' && coffeeInfo}
-        {simpleClock(new Date(), potsList || [], this.props.showCoffeePots !== 'false')}
+        {this.props.showCoffeePots !== "false" && coffeeInfo}
+        {simpleClock(
+          new Date(),
+          potsList || [],
+          this.props.showCoffeePots !== "false"
+        )}
       </Template>
     );
   }
