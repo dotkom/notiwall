fccabf954158a4d0f51f92a1f03a7a58dfab39da
Niklas Molnes Hole <nikasmh@hotmail.com>
2018-08-08 19:53:06 +0200

    Add a smartmirror style option

--- src/styles/index.js
+++ src/styles/index.js
@@ -8,6 +8,14 @@ const Online = `
   }
 `;
 
-export {
-  Online,
-};
+const SmartMirror = `
+, {
+  /*background-color: #121280;*/
+}
+
+.App {
+  /*background: #121280;*/
+}
+`;
+
+export { Online, SmartMirror };
